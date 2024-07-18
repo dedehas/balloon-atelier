b88d6a5a97cdf233d37d01889dddf2db93243a9c
dedehas <alesintex@gmail.com>
2024-07-18 19:02:02 +0100

    removed old footer from index and example/old comments from script, also dynamically fetch all stylesheets

--- footer.js
+++ footer.js
@@ -1,58 +1,14 @@
 const footerTemplate = document.createElement('template');
 
 footerTemplate.innerHTML = `
-  <style>
-    footer {
-      height: 60px;
-      padding: 0 10px;
-      list-style: none;
-      display: flex;
-      flex-shrink: 0;
-      justify-content: space-between;
-      align-items: center;
-    }
-
-    ul {
-      padding: 0;
-    }
-    
-    ul li {
-      list-style: none;
-      display: inline;
-    }
-    
-    a {
-      margin: 0 15px;
-      color: inherit;
-      text-decoration: none;
-    }
-    
-    a:hover {
-      padding-bottom: 5px;
-      box-shadow: inset 0 -2px 0 0 #333;
-    }
-    
-    .social-row {
-      font-size: 20px;
-    }
-    
-    .social-row li a {
-      margin: 0 15px;
-    }
-  </style>
-  <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
-    <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
-    <ul>
-      <li><a href="about.html">About</a></li>
-      <li><a href="work.html">Work</a></li>
-      <li><a href="contact.html">Contact</a></li>
-    </ul>
-    <ul class="social-row">
-      <li><a href="#"><i class="fa fa-google"></i></a></li>
-      <li><a href="https://www.instagram.com/balloonatelier_/"><i class="fa fa-instagram"></i></a></li>
-      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-    </ul>
-  </footer>
+<footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
+  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <a href="https://www.instagram.com/balloonatelier_/">
+      <i class="fa fa-instagram w3-hover-opacity"></i>
+    </a>
+  </div>
+</footer>
 `;
 
 class Footer extends HTMLElement {
@@ -64,12 +20,11 @@ class Footer extends HTMLElement {
   connectedCallback() {
     const shadowRoot = this.attachShadow({ mode: 'closed' });
 
-    // dynamically get font awesome from head
-    const fontAwesome = document.querySelector('link[href*="font-awesome"]');
-    if (fontAwesome) {
-      shadowRoot.appendChild(fontAwesome.cloneNode());
+    // dynamically get all stylesheets from head
+    const stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
+      if (stylesheets) {
+        stylesheets.forEach(x => shadowRoot.appendChild(x.cloneNode()));
     }
-
     shadowRoot.appendChild(footerTemplate.content);
   }
 }
