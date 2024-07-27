6ffd7eae89f11dc151ce1aaa89fa9345ca78e107
dedehas <alesintex@gmail.com>
2024-07-27 04:47:07 +0100

    why is js such a massive ballache

--- footer.js
+++ footer.js
@@ -1,16 +1,5 @@
 const footerTemplate = document.createElement('template');
 
-footerTemplate.innerHTML = `
-<footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
-  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
-  <div class="w3-xlarge w3-section">
-    <a href="https://www.instagram.com/balloonatelier_/">
-      <i class="fa fa-instagram w3-hover-opacity"></i>
-    </a>
-  </div>
-</footer>
-`;
-
 class Footer extends HTMLElement {
   constructor() {
     // inherit everything
@@ -25,7 +14,15 @@ class Footer extends HTMLElement {
       if (stylesheets) {
         stylesheets.forEach(x => shadowRoot.appendChild(x.cloneNode()));
     }
-    shadowRoot.appendChild(footerTemplate.content);
+
+    fetch("footer.html")
+      .then(response => response.text())
+      .then((response) => {
+        const data = response.toString();
+        footerTemplate.innerHTML = data;
+        shadowRoot.appendChild(footerTemplate.content);
+      });
+
   }
 }
 
