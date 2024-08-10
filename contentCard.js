afcd07b7dee916653b447ffc29277f427500d4d8
dedehas <alesintex@gmail.com>
2024-08-10 16:57:30 +0100

    content card template working

--- contentCard.js
+++ contentCard.js
@@ -1,42 +1,34 @@
 const cardTemplate = document.createElement('template')
 
-cardTemplate.innerHTML = `
-<style>
-  .card {
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    width: 40%;
-    border-radius: 5px;
-  }
-
-  .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-  }
-
-  img {
-    border-radius: 5px 5px 0 0;
-  }
-
-  .container {
-    padding: 2px 16px;
-  }
-</style>
-
-<div class="card">
-  <img src="images/balloon-icon.png" style="width:80%">
-  <div class="container">
-    <h4><b>Jane Doe</b></h4> 
-    <p>Interior Designer</p> 
-  </div>
-</div>
-`;
-
 class ContentCard extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({mode: 'open'});
-    this.shadowRoot.appendChild(cardTemplate.content.cloneNode(true))
+    // this.attachShadow({mode: 'open'});
+    // this.shadowRoot.appendChild(cardTemplate.content.cloneNode(true))
+  }
+
+  connectedCallback() {
+    // should mode be open or closed?
+    const shadowRoot = this.attachShadow({ mode: "closed" })
+
+    // dynamically get all stylesheets from head
+    const stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
+    if (stylesheets) {
+      stylesheets.forEach(x => shadowRoot.appendChild(x.cloneNode()));
+    }
+
+    fetch("/components/contentCard.html")
+      .then(response => response.text())
+      .then((response) => {
+        const data = response.toString();
+        cardTemplate.innerHTML = data;
+        shadowRoot.appendChild(cardTemplate.content);
+        // shadowRoot.appendChild(cardTemplate.content.cloneNode(true));
+      });
+
   }
 }
 
-window.customElements.define('content-card', ContentCard)
+// ? what does window do here is it necessary which one to use
+// window.customElements.define('content-card', ContentCard)
+customElements.define('content-card', ContentCard)
