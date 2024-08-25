d30ce5e738b6c1de8993194dff674eccac8ebcfe
dedehas <alesintex@gmail.com>
2024-08-25 17:56:24 +0100

    refactored navbar into lit element

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,13 @@
-export let navbarHTML = `
-<style>
-  nav {
+import {LitElement, css, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+
+export class Navbar extends LitElement {
+  static styles = css`
+    nav {
     position: fixed;
     top: 0;
     width: 100%;
     z-index: 1;
+    /* overflow-y: scroll; */
     background-color: var(--color-light-pink);
     border-bottom: 2px solid var(--color-light-grey);
     box-shadow: 0px 4px 16px var(--color-pink);
@@ -44,6 +47,7 @@ export let navbarHTML = `
     margin-inline-end: 8px;
   }
   a {
+    /* color: black; */
     text-decoration: none;
   }
 
@@ -64,34 +68,49 @@ export let navbarHTML = `
       visibility: hidden;
     }
   }
-</style>
-<nav id="nav" aria-label="Main">
-  <a href="/index.html" class="nav-button small">
-    <img src="/images/balloon-daisy-icon_clear.svg" style="height: 24px;">
-  </a>
-  <button id="menu-button" type="button" class="nav-button small" aria-label="Menu" aria-expanded="false" aria-controls="nav-links"><i class="fa fa-bars" style="margin-inline: 8px;" aria-hidden="true"></i></button>
+  `;
 
-  <div class="wrapper">
-    <ul role="list" id="nav-links">
-      <li>
-        <a href="/index.html" class="nav-button wide"><i class="fa fa-home"></i>HOME</a>
-      </li>
-      <li>
-        <a href="/pages/about.html" class="nav-button wide"><i class="fa fa-user" aria-hidden="true"></i>ABOUT</a>
-      </li>
-      <li>
-        <a href="/pages/services.html" class="nav-button wide"><i class="fa fa-th-list" aria-hidden="true"></i>SERVICES</a>
-      </li>
-      <li>
-        <a href="/pages/contact.html" class="nav-button wide"><i class="fa fa-envelope" aria-hidden="true"></i>CONTACT</a>
-      </li>
-      <li>
-        <a href="/pages/gallery.html" class="nav-button wide"><i class="fa fa-camera" aria-hidden="true"></i>GALLERY</a>
-      </li>
-      <li>
-        <a href="/pages/faq.html" class="nav-button wide"><i class="fa fa-question-circle" aria-hidden="true"></i>FAQ</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-`
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/style.css">
+    <nav id="nav" aria-label="Main">
+      <a href="/index.html" class="nav-button small">
+        <img src="/images/balloon-daisy-icon_clear.svg" style="height: 24px;">
+      </a>
+      <button id="menu-button" type="button" class="nav-button small" aria-label="Menu" aria-expanded="false" aria-controls="nav-links"><i class="fa fa-bars" style="margin-inline: 8px;" aria-hidden="true"></i></button>
+
+      <div class="wrapper">
+        <ul role="list" id="nav-links">
+          <li>
+            <a href="/index.html" class="nav-button wide"><i class="fa fa-home"></i>HOME</a>
+          </li>
+          <li>
+            <a href="/pages/about.html" class="nav-button wide"><i class="fa fa-user" aria-hidden="true"></i>ABOUT</a>
+          </li>
+          <li>
+            <a href="/pages/services.html" class="nav-button wide"><i class="fa fa-th-list" aria-hidden="true"></i>SERVICES</a>
+          </li>
+          <li>
+            <a href="/pages/contact.html" class="nav-button wide"><i class="fa fa-envelope" aria-hidden="true"></i>CONTACT</a>
+          </li>
+          <li>
+            <a href="/pages/gallery.html" class="nav-button wide"><i class="fa fa-camera" aria-hidden="true"></i>GALLERY</a>
+          </li>
+          <li>
+            <a href="/pages/faq.html" class="nav-button wide"><i class="fa fa-question-circle" aria-hidden="true"></i>FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    `;
+  }
+}
+
+customElements.define('nav-bar', Navbar)
