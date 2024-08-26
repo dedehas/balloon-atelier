1d7a8fe6a08bd6fd591777670914b735fa2d7191
dedehas <alesintex@gmail.com>
2024-08-26 02:08:13 +0100

    aria-current set for all pages in the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,10 @@
-import {LitElement, css, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import {LitElement, css, html, nothing} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
 
 export class Navbar extends LitElement {
 
   static properties = {
     active: {type: Boolean, reflect: true},
+    current: {type: String, reflect: true},
   }
 
   static styles = css`
@@ -83,15 +84,17 @@ export class Navbar extends LitElement {
   constructor() {
     super();
     this.active = false;  // bools must default to false
+    this.current = this.getAttribute("current");
     this.addEventListener("keyup", e => {
-      console.log(e.code);
       if (e.code === "Escape") {this.active = false}
     });
   }
 
-  _toggle(e) {this.active = !this.active; console.log(e)}
+  _toggle(e) {this.active = !this.active}
 
   _icon(status) {return status? "fa-close" : "fa-bars"}
+
+  _current(name) {return (name === this.current)? "page" : ""}
 
   render() {
     return html`
@@ -108,22 +111,22 @@ export class Navbar extends LitElement {
       <div class="wrapper">
         <ul role="list" id="nav-links">
           <li>
-            <a href="/index.html" class="nav-button wide"><i class="fa fa-fw fa-lg fa-home"></i>HOME</a>
+            <a href="/index.html" class="nav-button wide" aria-current=${this._current("index") || nothing}><i class="fa fa-fw fa-lg fa-home"></i>HOME</a>
           </li>
           <li>
-            <a href="/pages/about.html" class="nav-button wide"><i class="fa fa-fw fa-lg fa-user" aria-hidden="true"></i>ABOUT</a>
+            <a href="/pages/about.html" class="nav-button wide" aria-current=${this._current("about") || nothing}><i class="fa fa-fw fa-lg fa-user" aria-hidden="true"></i>ABOUT</a>
           </li>
           <li>
-            <a href="/pages/services.html" class="nav-button wide"><i class="fa fa-fw fa-lg fa-th-list" aria-hidden="true"></i>SERVICES</a>
+            <a href="/pages/services.html" class="nav-button wide" aria-current=${this._current("services") || nothing}><i class="fa fa-fw fa-lg fa-th-list" aria-hidden="true"></i>SERVICES</a>
           </li>
           <li>
-            <a href="/pages/contact.html" class="nav-button wide"><i class="fa fa-fw fa-lg fa-envelope" aria-hidden="true"></i>CONTACT</a>
+            <a href="/pages/contact.html" class="nav-button wide" aria-current=${this._current("contact") || nothing}><i class="fa fa-fw fa-lg fa-envelope" aria-hidden="true"></i>CONTACT</a>
           </li>
           <li>
-            <a href="/pages/gallery.html" class="nav-button wide"><i class="fa fa-fw fa-lg fa-camera" aria-hidden="true"></i>GALLERY</a>
+            <a href="/pages/gallery.html" class="nav-button wide" aria-current=${this._current("gallery") || nothing}><i class="fa fa-fw fa-lg fa-camera" aria-hidden="true"></i>GALLERY</a>
           </li>
           <li>
-            <a href="/pages/faq.html" class="nav-button wide"><i class="fa fa-fw fa-lg fa-question-circle" aria-hidden="true"></i>FAQ</a>
+            <a href="/pages/faq.html" class="nav-button wide" aria-current=${this._current("faq") || nothing}><i class="fa fa-fw fa-lg fa-question-circle" aria-hidden="true"></i>FAQ</a>
           </li>
         </ul>
       </div>
