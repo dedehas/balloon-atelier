cd0971c4a1ae84e45b5cb70ccd0435d3cb488219
dedehas <alesintex@gmail.com>
2024-09-03 16:51:41 +0100

    check button refactored with mixin

--- components/checkButton.js
+++ components/checkButton.js
@@ -1,37 +1,17 @@
-import {css, html, nothing} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
-import {HoverButton} from '/components/hoverButton.js';
+import {LitElement, html, nothing} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import { BaseButtonMixin } from '/components/baseButtonMixin.js';
 
-export class CheckButton extends HoverButton {
-
-  static properties = {
-    active: {type: Boolean}
-  }
-
-    static styles = super.styles;
+export class CheckButton extends BaseButtonMixin(LitElement) {
 
   constructor() {
     super();
-    // no href
-    this.msg = this.getAttribute("msg");
-    this.solo = this.msg ? "" : "solo";
-    this.iconOff = this.getAttribute("icon-off") ?? "fa-bars";
-    this.iconOn = this.getAttribute("icon-on") ?? "fa-close";
-    // no src
-    this.active = false; // bools default to false
+    this.iconChecked = this.getAttribute("icon-checked");
     this.addEventListener("keyup", e => {
       if (e.code === "Escape") {this.active = false}
     });
   }
 
-  _icon(status) {return status? this.iconOn : this.iconOff}
-
-  iconTemplate() {
-    return html`<i class="fa fa-fw fa-lg ${this._icon(this.active)} ${this.solo}" aria-hidden="true"></i>`;
-  }
-
-  //no imgTemplate
-
-  // no anchorTemplate
+  _icon(status) {return status? this.iconChecked : this.icon}
 
   _toggle() {this.active = !this.active}
 
@@ -43,11 +23,7 @@ export class CheckButton extends HoverButton {
   }
 
   render() {
-    return html`
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/style.css">
-    ${this.buttonTemplate()}
-    `;
+    return this.renderHelper(html`${this.buttonTemplate()}`);
   }
 }
 
